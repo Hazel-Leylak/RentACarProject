4c9ec0cf8324e9345ee0f1d833b8c35daa6cfaa2
Hazel Leylak <hazel.leylak@gmail.com>
2021-02-07 18:04:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemoryMode;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryMode/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemoryMode
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=2,CarName="Hyundai i20",DailyPrice=220000,Description="**informations**",ModelYear="2020"},
+                new Car{CarId=1,BrandId=2,ColorId=2,CarName="Honda Civic",DailyPrice=264000,Description="**informations**",ModelYear="2021"},
+                new Car{CarId=1,BrandId=3,ColorId=2,CarName="Peugeot 308",DailyPrice=133500,Description="**informations**",ModelYear="2014"},
+                new Car{CarId=1,BrandId=4,ColorId=2,CarName="BMW 320i",DailyPrice=108500,Description="**informations**",ModelYear="1999"},
+                new Car{CarId=1,BrandId=4,ColorId=2,CarName="BMW 418i Gran Coupe Prestige",DailyPrice=354000,Description="**informations**",ModelYear="2015"},
+                new Car{CarId=1,BrandId=5,ColorId=2,CarName="Mercedes Benz C 200 d BlueTEC Exclusive ",DailyPrice=482000,Description="**informations**",ModelYear="2017"},
+                new Car{CarId=1,BrandId=6,ColorId=2,CarName="Honda Accord 2.0 ES",DailyPrice=47900,Description="**informations**",ModelYear="1997"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carsToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carsToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarName = car.CarName;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
