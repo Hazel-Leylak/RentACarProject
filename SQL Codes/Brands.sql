5e5e78a0c450c38cc5b48f06184583b4b2bab5b6
Hazel <47564151+Hazel-Leylak@users.noreply.github.com>
2021-02-13 20:00:38 +0300

    Delete SQL Codes directory

--- SQL Codes/Brands.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[Brands] (
-    [BrandId]   INT  IDENTITY (1, 1) NOT NULL,
-    [BrandName] TEXT NOT NULL,
-    PRIMARY KEY CLUSTERED ([BrandId] ASC)
-);
-
--- SQL Codes/Colors.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[Colors] (
-    [ColorId]   INT  IDENTITY (1, 1) NOT NULL,
-    [ColorName] TEXT NOT NULL,
-    PRIMARY KEY CLUSTERED ([ColorId] ASC)
-);
-
